9358aa21a3d81463af5b5b9e7bd9df747412bf17
yangluoshen <649673800@qq.com>
2015-03-09 23:40:13 +0800

    add load activity

--- src/com/Loktar/lovemanager/LoadActivity.java
+++ src/com/Loktar/lovemanager/LoadActivity.java
@@ -1,15 +1,40 @@
 package com.Loktar.lovemanager;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LoadActivity extends Activity {
+	
+	private static final int LOAD_DISPLAY_TIME = 1500;
+	
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		getWindow().setFormat(PixelFormat.RGBA_8888);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_DITHER);
 		setContentView(R.layout.load_layout);
+		
+		new Handler().postDelayed(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				Intent mainIntent = new Intent(LoadActivity.this,MainActivity.class);
+				LoadActivity.this.startActivity(mainIntent);
+				LoadActivity.this.finish();
+				
+			}
+			
+		}, LOAD_DISPLAY_TIME);
 	}
 
 	@Override
---
+++ src/com/Loktar/lovemanager/MainActivity.java
@@ -0,0 +1,16 @@
+package com.Loktar.lovemanager;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onCreate(arg0);
+		setContentView(R.layout.main_layout);
+	}
+	
+
+}
